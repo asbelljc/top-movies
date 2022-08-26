89ab09d2b6a3329936ebc2d4c95da5a1303e3e8f
Jonathan Asbell <asbelljc@gmail.com>
2022-08-26 01:16:17 -0400

    Added code for adding new movies and redirecting to edit page.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import os
+import requests
+
+from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-import requests
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
@@ -38,6 +44,11 @@ class RateMovieForm(FlaskForm):
     submit = SubmitField("Done")
 
 
+# New movie form
+class FindMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+
+
 @app.route("/")
 def home():
     all_movies = Movie.query.all()
@@ -68,6 +79,48 @@ def delete_movie():
     db.session.commit()
 
     return redirect(url_for("home"))
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(
+            "https://api.themoviedb.org/3/search/movie",
+            params={"api_key": API_KEY, "query": movie_title},
+        )
+        data = response.json()["results"]
+
+        return render_template("select.html", options=data)
+
+    return render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"https://api.themoviedb.org/3/movie/{movie_api_id}"
+        response = requests.get(
+            movie_api_url, params={"api_key": API_KEY, "language": "en-US"}
+        )
+        data = response.json()
+        print(
+            f"https://api.themoviedb.org/3/movie/{movie_api_id}/images{data['poster_path']}"
+        )
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"https://image.tmdb.org/t/p/original{data['poster_path']}",
+            description=data["overview"],
+        )
+
+        db.session.add(new_movie)
+        db.session.commit()
+
+        return redirect(url_for("rate_movie", id=new_movie.id))
 
 
 if __name__ == "__main__":
